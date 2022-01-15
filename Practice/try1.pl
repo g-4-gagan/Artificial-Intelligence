0343ccf7350006f9e90f8b547e56ea5a848b5868
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-01-15 22:55:57 +0530

    trying family tree problem

--- Practice/try1.pl
+++ Practice/try1.pl
@@ -11,15 +11,36 @@ parent(bob,pam).
 parent(luna,pam).
 parent(pam,tom).
 parent(jenny,tom).
+parent(pam,ron).
+parent(jenny,ron).
+parent(jack,perry).
+parent(dua,perry).
+parent(jack,pammy).
+parent(dua,pammy).
+parent(tom,daya).
+parent(perry,daya).
+parent(tom,shawn).
+parent(perry,shawn).
+parent(pammy,john).
+parent(roch,john).
 
 %male fact
 male(bob).
 male(pam).
 male(tom).
+male(ron).
+male(jack).
+male(shawn).
+male(roch).
+male(john).
 
 %female fact
 female(luna).
 female(jenny).
+female(dua).
+female(pammy).
+female(perry).
+female(daya).
 
 %husband rule
 husband(X,Y):-
@@ -32,6 +53,20 @@ wife(X,Y):-
 	parent(X,Z),
 	parent(Y,Z),
 	female(X).
+
+%child rule
+child(X,Y):-
+	parent(Y,X).
+
+%son rule
+son(X,Y):-
+	parent(Y,X)
+	male(X).
+
+%daughter rule
+daughter(X,Y):-
+	parent(Y,X)
+	female(Y).
 
 %rule father
 father(X,Y):-
