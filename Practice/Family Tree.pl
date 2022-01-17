a1cf4413dba311b7247b7d2305e087dff9dc7ebb
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-01-18 00:32:09 +0530

    family tree and math operations

---
+++ Practice/Family Tree.pl
@@ -0,0 +1,170 @@
+%First Try Program (single line comment)
+
+/*
+make the pile of features and place each at same place
+make rules using the features given and then consult
+to ask questions.
+*/
+
+% Parent fact
+parent(bob,pam).
+parent(luna,pam).
+parent(pam,tom).
+parent(jenny,tom).
+parent(pam,ron).
+parent(jenny,ron).
+parent(jack,perry).
+parent(dua,perry).
+parent(jack,pammy).
+parent(dua,pammy).
+parent(tom,daya).
+parent(perry,daya).
+parent(tom,shawn).
+parent(perry,shawn).
+parent(pammy,john).
+parent(roch,john).
+
+%male fact
+male(bob).
+male(pam).
+male(tom).
+male(ron).
+male(jack).
+male(shawn).
+male(roch).
+male(john).
+
+%female fact
+female(luna).
+female(jenny).
+female(dua).
+female(pammy).
+female(perry).
+female(daya).
+
+%spouse fact
+spouse(bob,luna).
+spouse(pam,jenny).
+spouse(tom,perry).
+spouse(jack,dua).
+spouse(pammy,roch).
+
+%spouse rule
+spouse(X,Y):-
+	spouse(Y,X).
+
+%husband rule
+husband(X,Y):-
+	/*parent(X,Z),
+	parent(Y,Z),
+	male(X),
+	X\=Y.*/
+	wife(Y,X).
+
+%wife rule
+wife(X,Y):-
+	parent(X,Z),
+	parent(Y,Z),
+	female(X),
+	X\=Y.
+	%husband(Y,X).
+
+%child rule
+child(X,Y):-
+	parent(Y,X).
+
+%son rule
+son(X,Y):-
+	parent(Y,X),
+	male(X).
+
+%daughter rule
+daughter(X,Y):-
+	parent(Y,X),
+	female(Y).
+
+%father rule
+father(X,Y):-
+	parent(X,Y),
+	male(X).
+
+%mother rule
+mother(X,Y):-
+	parent(X,Y),
+	female(X).
+
+%GrandParent rule
+grandparent(X,Y):-
+	parent(X,Z),
+	parent(Z,Y).
+
+%GrandFather rule
+grandfather(X,Y):-
+	parent(X,Z),
+	parent(Z,Y),
+	male(X).
+
+%GrandMother rule
+grandmother(X,Y):-
+	parent(X,Z),
+	parent(Z,Y),
+	female(X).
+
+%GrandChild rule
+grandchild(X,Y):-
+	grandparent(Y,X).
+
+%GrandSon rule
+grandson(X,Y):-
+	grandparent(Y,X),
+	male(X).
+
+%GrandDaughter rule
+granddaughter(X,Y):-
+	grandparent(Y,X),
+	female(X).
+
+%Ancestor rule
+
+%Sibling rule
+sibling(X,Y):-
+	parent(Z,X),
+	parent(Z,Y),
+	X\=Y.
+
+%Sister rule
+sister(X,Y):-
+	parent(Z,X),
+	parent(Z,Y),
+	X\=Y,
+	female(X).
+
+%Brother rule
+brother(X,Y):-
+	parent(Z,X),
+	parent(Z,Y),
+	X\=Y,
+	male(X).
+
+%Uncle rule
+uncle(X,Y):-
+	parent(Z,Y),
+	sibling(X,Z),
+	X\=Z,
+	male(X).
+
+%Aunt rule
+aunt(X,Y):-
+	parent(Z,Y),
+	sibling(X,Z),
+	X\=Z,
+	female(X).
+
+%Nephew rule
+
+%Niece rule
+
+%Cousin rule
+
+
+
