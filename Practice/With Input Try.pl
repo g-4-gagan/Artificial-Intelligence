c865b145aa4cb378291c9c1657520d14b70c5e19
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-01-21 01:35:21 +0530

    prolog input output try

---
+++ Practice/With Input Try.pl
@@ -0,0 +1,28 @@
+%sum(X,Y,R):-R is X+Y.
+
+sum:-
+	write("Enter First number: "),
+	read(X),
+	write("Enter Second number"),
+	read(Y),
+	Z is X+Y,
+	write("Sum of "),write(X),
+	write(" and "),write(Y),
+	write(" is "),write(Z),
+	nl, cont.
+
+cont:-
+	write("Do you want to do some more sum(y/n)? "),
+  	read(X),
+	check(X).
+
+check(y):-
+	sum.
+
+check(n):-
+	!.
+
+check(X):-
+	write("Please choose right choice "),
+	nl,cont.
+	
