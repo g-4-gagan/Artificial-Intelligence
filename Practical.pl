e9f7808ac5e6fd3f800841e854593087b9ed4118
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-01-24 22:48:27 +0530

    prcatical 2

--- Practical.pl
+++ Practical.pl
@@ -2,3 +2,14 @@
 Write a prolog program to calculate the sum of two numbers.*/
 sum(X,Y,R):-
 	R is X+Y.
+
+/*Practical 2
+Write a Prolog program to implement max(X, Y, M) so that M is the maximum of two numbers X and Y.*/
+max(X,Y,M):-
+	X>Y,
+	M=X.
+
+max(X,Y,M):-
+	Y>=X,
+	M=Y.
+
