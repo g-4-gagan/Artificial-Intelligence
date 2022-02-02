232ea97087a932875e351bb8be4107883b4d3d69
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-03 01:21:28 +0530

    Practical completed upto 12

--- Practical(8-12).pl
+++ Practical(8-12).pl
@@ -9,3 +9,24 @@ Write a Prolog program to implement conc (L1, L2, L3) where L2 is the list to be
 conc([],L,L).
 conc([X|L1],L2,[X|L3]):-
 	conc(L1,L2,L3).
+
+/*Practical 10
+Write a Prolog program to implement reverse (L, R) where List L is original and List R is reversed list.*/
+reverse([],[]).
+reverse([X|L1],L2):-
+	reverse(L1,L3),
+	conc(L3,[X],L2).
+
+/*Practical 11
+Write a program in PROLOG to implement palindrome (L) which checks whether a list L is a palindrome or not.*/
+palindrome(L):-
+	reverse(L,L).
+
+/*Practical 12
+Write a Prolog program to implement sumlist(L, S) so that S is the sum of a given list L.*/
+sumlist([],0).
+sumlist([X|L1],S):-
+	sumlist(L1,S1),
+	S is S1+X.
+	
+
