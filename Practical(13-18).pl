dc0b7ad5b181a48515d244a23719bb5fd8ad9e17
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-10 22:03:48 +0530

    Practical 13 and 14

--- Practical(13-18).pl
+++ Practical(13-18).pl
@@ -1,4 +1,15 @@
 /*Practical 13
 Write a Prolog program to implement two predicates evenlength(List) and oddlength(List) so that they are true if their argument is a list of even or odd length respectively.*/
+evenlength([]).
+evenlength([_|T]):-
+	oddlength(T).
+oddlength([_|T]):-
+	evenlength(T).
 
+/*Practical 14
+Write a Prolog program to implement nth_element (N, L, X) where N is the desired position, L is a list and X represents the Nth element of L.*/
+nth_element(1, [X|_], X).
+nth_element(K,[_|L],X):-
+	nth_element(K1,L,X),
+	K is K1+1.
 
