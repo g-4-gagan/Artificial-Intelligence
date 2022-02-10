ccd9cb4de1c7ba446d4d65149ee61bc6cb9c9688
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-10 22:15:57 +0530

    Practical 18

--- Practical(13-18).pl
+++ Practical(13-18).pl
@@ -35,5 +35,16 @@ delete_nth(N, [H|T], [H|R]):-
 	N1 is N-1,
 	delete_nth(N1, T, R).
 
+/*Practical 18
+Write a program in PROLOG to implement merge (L1, L2, L3) where L1 is first ordered list and L2 is second ordered list and L3 represents the merged list.*/
+merge([],L2,L2).
+merge(L1,[],L1).
+merge([H1|T1],[H2|T2],[H1|T3]):- 
+	H1=<H2,
+	merge(T1, [H2|T2], T3).
+merge([H1|T1],[H2|T2],[H2|T3]):- 
+	merge([H1|T1], T2, T3).
+
+
 
 
