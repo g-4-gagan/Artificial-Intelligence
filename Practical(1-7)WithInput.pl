d3b7df99bb4dec0d782aeb445bc5d7a1bffe1408
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-16 22:22:58 +0530

    Practical 2 with input

--- Practical(1-7)WithInput.pl
+++ Practical(1-7)WithInput.pl
@@ -25,4 +25,19 @@ check(n):-
 check(X):-
 	write("Please choose right choice "),
 	nl,cont.
+
+/*Practical 2
+Write a Prolog program to implement max(X, Y, M) so that M is the maximum of two numbers X and Y.*/
+max:-
+	write("Enter First number: "),
+	read(X),
+	write("Enter Second number"),
+	read(Y),
+	(  X =< Y
+    	-> Z = Y
+    	;  Z = X
+     	),write("Maximum between "),write(X),
+	write(" and "),write(Y),
+	write(" is "),write(Z),
+	!.
 	
