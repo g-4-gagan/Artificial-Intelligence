90bf76502c1e9a2d7b8296542c4da48371f80577
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-20 22:11:17 +0530

    Practical 9 with input

--- Practical(8-12)WithInput.pl
+++ Practical(8-12)WithInput.pl
@@ -12,3 +12,17 @@ member:-
 	(memb(X,L)->
 	write(" is the member of the given list")
 	;write(" is not the member of the given list")),!.
+
+/*Practical 9
+Write a Prolog program to implement conc (L1, L2, L3) where L2 is the list to be appended with L1 to get the resulted list L3.*/
+conc([],L,L).
+conc([X|L1],L2,[X|L3]):-
+	conc(L1,L2,L3).
+concatenate:-
+	write("Enter First List: "),
+	read(L1),
+	write("Enter Second List: "),
+	read(L2),
+	conc(L1,L2,L3),
+	write("The concatenated list is: "),
+	write(L3),!.
