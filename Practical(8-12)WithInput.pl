94b63550ba7ed234e528026913b6544fd5ebfe8c
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-20 21:54:31 +0530

    Practical 8 with input

--- Practical(8-12)WithInput.pl
+++ Practical(8-12)WithInput.pl
@@ -3,4 +3,12 @@ Write a Prolog program to implement memb(X, L): to check whether X is a member o
 memb(X,[X|T]).
 memb(X,[H|T]):-
 	memb(X,T).
-member:-.
+member:-
+	write("Enter the List: "),
+	read(L),
+	write("Enter the number to be found: "),
+	read(X),
+	write(X),
+	(memb(X,L)->
+	write(" is the member of the given list")
+	;write(" is not the member of the given list")),!.
