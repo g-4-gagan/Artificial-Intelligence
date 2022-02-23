6ac45b06771e20413599452fe937ff72c22a6023
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-24 00:32:29 +0530

    maximum element in the list with input

--- Practical(13-18)WithInput.pl
+++ Practical(13-18)WithInput.pl
@@ -27,3 +27,19 @@ nth_element:-
 	write("The element at position "),
 	write(N),write(" in the list is: "),
 	write(X),!.
+
+/*Practical 15
+Write a Prolog program to implement maxlist(L, M) so that M is the maximum number in the list.*/
+maxlist([X],X).
+maxlist([H|T],M):- 
+	maxlist(T,M1), 
+	H<M1 -> M is M1;
+	M is H.
+maxlist:-
+	write("Enter the list: "),
+	read(L),
+	maxlist(L,X),
+	write("The maximum element in the given list is: "),
+	write(X),!.
+
+
