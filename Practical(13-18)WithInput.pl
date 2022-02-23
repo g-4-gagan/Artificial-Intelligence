45a4dd4da77056f1bef92981a818f8d3ea595b0e
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-24 00:26:37 +0530

    practical 14 nth element with input

--- Practical(13-18)WithInput.pl
+++ Practical(13-18)WithInput.pl
@@ -11,3 +11,19 @@ evenoddlength:-
 	(evenlength(L)
 	->write("The entered list is even length");
 	write("The entered list is odd length")),!.
+
+/*Practical 14
+Write a Prolog program to implement nth_element (N, L, X) where N is the desired position, L is a list and X represents the Nth element of L.*/
+nth_element(1, [X|_], X).
+nth_element(K,[_|L],X):-
+	nth_element(K1,L,X),
+	K is K1+1.
+nth_element:-
+	write("Enter the list: "),
+	read(L),
+	write("Enter the position of the element"),
+	read(N),
+	nth_element(N,L,X),
+	write("The element at position "),
+	write(N),write(" in the list is: "),
+	write(X),!.
