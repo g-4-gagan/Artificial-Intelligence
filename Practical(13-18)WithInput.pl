af001b5c27389eaaf6d483f446118ce73510487e
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-25 21:22:09 +0530

    13-18 with input complete

--- Practical(13-18)WithInput.pl
+++ Practical(13-18)WithInput.pl
@@ -77,3 +77,20 @@ delete_nth:-
 	write(N),write(" position in the list is: "),
 	write(R),!.
 
+/*Practical 18
+Write a program in PROLOG to implement merge (L1, L2, L3) where L1 is first ordered list and L2 is second ordered list and L3 represents the merged list.*/
+merge([],L2,L2).
+merge(L1,[],L1).
+merge([H1|T1],[H2|T2],[H1|T3]):- 
+	H1=<H2,
+	merge(T1, [H2|T2], T3).
+merge([H1|T1],[H2|T2],[H2|T3]):- 
+	merge([H1|T1], T2, T3).
+merge:-
+	write("Enter the first ordered list: "),
+	read(L1),
+	write("Enter the second ordered list: "),
+	read(L2),
+	merge(L1,L2,L3),
+	write("The final list after merging given two ordered list: "),
+	write(L3),!.
